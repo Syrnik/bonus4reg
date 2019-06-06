d88f88a821141263f3bf7a853d461e678b0d4e2e
Serge Rodovnichenko <sergerod@gmail.com>
2019-06-06 12:32:50 +0300

    Подписался на события в новом формате

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -2,19 +2,23 @@
 /**
  * @package Bonus4reg
  * @author Serge Rodovnichenko <serge@syrnik.com>
- * @version 1.0.0
- * @copyright (c) 2014, Serge Rodovnichenko
+ * @copyright (c) 2014-2019, Serge Rodovnichenko
  * @license http://www.webasyst.com/terms/#eula Webasyst
  */
 return array(
-    'name' => _wp('Signup Bonus'),
-    'img' => 'img/icon.png',
-    'version' => '1.0.0',
-    'vendor' => '670917',
-    'shop_settings' => TRUE,
-    'handlers' =>
-    array(
-        'backend_settings_affiliate' => 'backendSettingsAffiliate',
-        'signup' => 'signupHandler'
-    ),
+    'name'          => _wp('Signup Bonus'),
+    'img'           => 'img/icon.png',
+    'version'       => '1.0.0',
+    'vendor'        => '670917',
+    'shop_settings' => true,
+    'handlers'      =>
+        array(
+            'backend_settings_affiliate' => 'backendSettingsAffiliate',
+            '*'                          => array(
+                'event_app_id' => '*',
+                'event'        => 'signup',
+                'class'        => 'shopBonus4regPlugin',
+                'method'       => 'signupHandler'
+            )
+        ),
 );
