d789f8e48b0354364742e42b21bf8ee03a256fb2
Serge Rodovnichenko <sergerod@gmail.com>
2019-06-06 12:08:27 +0300

    Требуем фреймворк 1.10+

---
+++ lib/config/requirements.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Serge Rodovnichenko <serge@syrnik.com>
+ * @copyright Serge Rodovnichenko, 2019
+ * @license http://www.webasyst.com/terms/#eula Webasyst
+ */
+
+return array(
+    'app.installer'          => array(
+        'version' => '>=1.10', 'strict' => true,
+    ),
+);
