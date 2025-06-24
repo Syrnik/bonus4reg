92a28b5799b514e32bd54d77fb01f726dd052cff
Serge Rodovnichenko <sergerod@gmail.com>
2025-06-24 12:08:13 +0300

    Fix UI 1.3 template #67.1

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -8,7 +8,7 @@
 return array(
     'name'          => /*_wp*/'Signup Bonus',
     'img'           => 'img/icon.png',
-    'version'       => '2.0.0',
+    'version'       => '2.0.1',
     'vendor'        => '670917',
     'shop_settings' => true,
     'handlers'      =>
